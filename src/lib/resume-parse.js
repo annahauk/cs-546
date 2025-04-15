53d70984bc57ed0170bb80092022ce9e41a8f619
KCGD <kcgdhosting@gmail.com>
2025-04-15 12:20:54 -0400

    added nlp library and base for resume parser

---
+++ src/lib/resume-parse.js
@@ -0,0 +1,15 @@
+/**
+ * pdftotext --> poppler-utils
+
+json: terms[n].tags
+    Name: ProperNoun, FirstName, Person
+    Locations: Place, Region
+    Numbers: NumericValue, Cardinal, NumericValue, NumberRange
+    Emails: Email,
+    Urls: Url
+    orgs: Organization
+    Experiences: Date, Noun, Month
+    Skills: Expression, 
+ */
+
+import * as nlp from "compromise";
