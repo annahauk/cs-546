f7083c6ee1c8785dcba19a4731a2b5b3cc7d3563
KCGD <kcgdhosting@gmail.com>
2025-04-23 22:29:06 -0400

    fix auth route and add body parsing

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 // import somethingRoutes from 'somewhere/someroutes.js'
+import * as express from "express";
 import AuthRoutes from "./authroutes.js"
 
 export function constructorMethod(app) {
@@ -6,10 +7,12 @@ export function constructorMethod(app) {
         throw new Error(`App is not defined!`);
     }
 
+    app.use(express.json());
+
     // app.use('/route', somethingRoutes)
 
     // auth routes
-    app.use("/route", AuthRoutes);
+    app.use("/auth", AuthRoutes);
 
     app.use('*', (req, res) => {
         return res.status(404).json({error: 'Not found'});
