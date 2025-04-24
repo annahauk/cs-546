3618d8c0b67f282a0721089f62708426c4ddb57f
KCGD <kcgdhosting@gmail.com>
2025-04-23 22:28:54 -0400

    add auth route

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -1,4 +1,6 @@
 import * as express from "express";
+import { stringVal } from "../helpers.js";
+import { login } from "../src/lib/auth.js";
 
 const router = express.Router();
 
@@ -11,7 +13,48 @@ const router = express.Router();
  */
 router.route("/login")
     .post(async (req,res) => {
-        
+        let token = "";
+        if(typeof req.body !== "object") {
+            return await res.status(400).json({"error": `Request body required.`});
+        }
+
+        // validate username and password
+        let username = req.body["username"];
+        let password = req.body["password"];
+        try {
+            stringVal(username);
+            stringVal(password);
+        } catch (e) {
+            return await res.status(400).json({error: `Invalid username or password.`});
+        }
+
+        // attempt login
+        try {
+            token = await login(username, password);
+        } catch (e) {
+            return await res.status(500).json({error: `Error occured while logging in.`});
+        }
+
+        // if token undefined, login was failed
+        if(!token) {
+            return await res.status(401).json({error: `Incorrect username or password.`});
+        }
+
+        // success, return token
+        // TODO: Enforce secure once on https
+        res.cookie('token', token, {
+            httpOnly: true,     // prevents JavaScript access
+            //secure: true,       // send only over HTTPS
+            sameSite: 'lax',    // CSRF protection
+            maxAge: 86400000*28    // 1 month
+        });
+        res.cookie('username', username, {
+            httpOnly: true,     // prevents JavaScript access
+            //secure: true,       // send only over HTTPS
+            sameSite: 'lax',    // CSRF protection
+            maxAge: 86400000*28    // 1 month
+        });
+        res.status(200).json({message: "Logged in."});
     })
 
 export default router;
