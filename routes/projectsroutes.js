a264074b40c56fd751288ed903f8a91a9e2ce670
cskal <cskalish@gmail.com>
2025-05-04 13:00:31 -0400

    Restructured routing to root

--- routes/projectsroutes.js
+++ routes/projectsroutes.js
@@ -1,13 +1,23 @@
 import {Router} from 'express';
 const router = Router();
 import {getAllPosts, getPostById} from "../data/posts.js";
-import {isLoggedIn} from "./middleware.js";
+import { getUserByUsername } from '../data/users.js';
+import { createComment } from "../data/comments.js";
+import { isLoggedIn } from "./middleware.js";
+import { stringVal } from '../helpers.js';
 
 router.route('/')
     .get(isLoggedIn, async (req, res) => {
         try {
-            const allPosts = await getAllPosts();
-            res.render('project', {posts: allPosts});
+            // Filter by tags, languages, or active/inactive
+            let tags = req.query;
+            let allPosts = await getAllPosts();
+            if (tags) {
+                // trim and lowercase may be redundant but we can simplify later
+                tags = tags.split(',').map(tag => tag.trim().toLowerCase());
+                allPosts = allPosts.filter(post => post.topic_tags.some(tag => tags.includes(tag.toLowerCase())));
+            }
+            res.render('projects', {posts: allPosts});
         } catch (error) {
             console.error(error);
             res.status(500).render('error', {message: 'Internal server error'});
@@ -22,7 +32,40 @@ router.route('/:id')
             if (!post) {
                 return res.status(404).render('error', {message: 'Project not found'});
             }
-            res.render('project', {post: post});
+            res.render('projectDetails', {post: post});
+        } catch (error) {
+            console.error(error);
+            res.status(500).render('error', {message: 'Internal server error'});
+        }
+    }).post(isLoggedIn, async (req, res) => {
+        try {
+            const projectId = req.params.id;
+            const {action, content} = req.body;
+            action = stringVal(action);
+            content = stringVal(content);
+        } catch (error) {
+            console.error(error);
+            return res.status(400).json({message: error});
+        }
+
+        try {
+            const post = await getPostById(projectId);
+            const user = await getUserByUsername(req.body.username);
+            if (!post) {
+                return res.status(404).render('error', {message: 'Project not found'});
+            }
+            if (!user) {
+                return res.status(404).render('error', {message: 'User not found'});
+            }
+            if (action === 'comment') {
+                await createComment(content, projectId, user._id);
+                return res.status(200).json({ message: 'Comment added successfully' });
+            }
+            else if (action === 'join') {
+                // Implement join functionality
+            } else {
+                return res.status(400).render('error', {message: 'Invalid action'});
+            }
         } catch (error) {
             console.error(error);
             res.status(500).render('error', {message: 'Internal server error'});
