724b10c588e16499cba9957871df5ff8b2e414a4
cskal <cskalish@gmail.com>
2025-05-04 12:14:20 -0400

    Deleted a comment (sorry it was bothering me i misclicked it in my previous commit)

--- routes/index.js
+++ routes/index.js
@@ -7,20 +7,6 @@ import HomeRoutes from "./homeroutes.js";
 import ProfileRoutes from "./profileroutes.js";
 import ProjectsRoutes from "./projectsroutes.js";
 
-/*
-you will be making three routes/pages in your application:
-
-http://localhost:3000/  this is the homepage where users can either create an account or login to their existing account
-http://localhost:3000/login  this is the login page where users can enter their credentials to log in
-http://localhost:3000/home when logged in, users will be brought to a homepage where they can see the main interface page
-http://localhost:3000/profile  users can click on one of the buttons at the header to see their profile
-http://localhost:3000/
-
-
-*/
-
-
-
 export function constructorMethod(app) {
     if(typeof app === "undefined") {
         throw new Error(`App is not defined!`);
