7f5d0a6fa565211dea0d21a98e670d430c2116f4
ZakariyyaScavotto <zakscav4@gmail.com>
2025-05-10 16:26:24 -0400

    started notifications routing

---
+++ routes/notifications.js
@@ -0,0 +1,33 @@
+import { Router } from "express";
+import { isLoggedIn } from "./middleware.js";
+import { getUserById, getUserByUsername } from "../data/users.js";
+import { idVal, stringVal } from "../helpers.js";
+
+const router = Router();
+
+router.route("/").get(isLoggedIn, async (req, res) => {
+	if (req.authorized) {
+		let username = req.cookies["username"];
+		let user = await getUserByUsername(username);
+		return res.redirect("/notifications/" + user._id);
+	} else {
+		return res.redirect("/login");
+	}
+});
+
+router.route("/:id").get(isLoggedIn, async (req, res) => {
+	// Display a user's notifications
+	try {
+		const userId = idVal(req.params.id);
+		const user = await getUserById(userId);
+		if (!user) {
+			return res.status(404).render("error", { message: "User not found" });
+		}
+		res.render("notifications", { user: user });
+	} catch (error) {
+		console.error(error);
+		res.status(500).render("error", { message: "Internal server error" });
+	}
+});
+
+export default router;
