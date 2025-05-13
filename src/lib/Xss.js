aac7596738f509ac057fb783ed3ae91c31d1aed5
KCGD <kcgdhosting@gmail.com>
2025-05-13 18:41:27 -0400

    implement XSS for helpers and middleware (body and cookies)

---
+++ src/lib/Xss.js
@@ -0,0 +1,25 @@
+import { xss } from "../../helpers.js";
+
+export async function Xss(req,res,next) {
+    /**
+     * sanitize cookie inputs
+     */
+    if(req.cookies) {
+        const cookie_keys = Object.keys(req.cookies);
+        for(const key of cookie_keys) {
+            req.cookies[key] = xss(req.cookies[key]);
+        }
+    }
+
+    /**
+     * sanitize body inputs
+     */
+    if(req.body) {
+        const bodykeys = Object.keys(req.body);
+        for(const key of bodykeys) {
+            req.body[key] = xss(req.body[key]);
+        }
+    }
+
+    next();
+}
